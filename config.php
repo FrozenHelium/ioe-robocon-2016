08f89c0093c16c6ea46a7d508d29e458356733d4
Frozen Helium <fren.ankit@gmail.com>
2016-03-18 21:47:25 +0545

    Add rulebook

--- config.php
+++ config.php
@@ -3,10 +3,17 @@
 // config.php
 // contains global configurations
 
-define("HOST",      "robocon.ioe.edu.np");
-define("USER",      "robocon");
-define("PASSWORD",  "XQCo7NdeX5UZnGtR");
+// define("HOST",      "robocon.ioe.edu.np");
+// define("USER",      "robocon");
+// define("PASSWORD",  "XQCo7NdeX5UZnGtR");
+// define("DATABASE",  "robocon2016");
+
+define("HOST",      "localhost");
+define("USER",      "root");
+define("PASSWORD",  "root");
 define("DATABASE",  "robocon2016");
+
+
 
 define("ROOTDIR", __DIR__);
 
